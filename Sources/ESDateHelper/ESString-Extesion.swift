52b60157dc93ab40e184a39ce99b7d89f6271699
Emil Karimov <emvakar@gmail.com>
2020-06-12 02:13:20 +0300

    [in-progress] date extesnion

---
+++ Sources/ESDateHelper/ESString-Extesion.swift
@@ -0,0 +1,19 @@
+//
+//  ESString+Extension.swift
+//  ESDateHelper
+//
+//  Created by Emil Karimov on 12/06/2020
+//  Copyright © 2020 ESKARIA LLC. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func toDate(fromFormat: DateFormatType) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = fromFormat.stringFormat
+        return formatter.date(from: self)
+    }
+    
+}
