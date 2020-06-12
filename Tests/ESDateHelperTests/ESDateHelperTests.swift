3e709f92cbfb9be4bf2d6c87124bba474374693f
Emil Karimov <emvakar@gmail.com>
2020-06-12 16:01:32 +0300

    [in-progress] add same date test case

--- Tests/ESDateHelperTests/ESDateHelperTests.swift
+++ Tests/ESDateHelperTests/ESDateHelperTests.swift
@@ -2,13 +2,20 @@ import XCTest
 @testable import ESDateHelper
 
 final class ESDateHelperTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-    }
-
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testSameDate", testSameDate),
     ]
+    
+    func testSameDate() {
+        let dateString = "2020-06-12T02:42+0300"
+        let date = Date(fromString: dateString, format: .isoDateTime)
+        let dateToString = date?.toString(format: .isoDateTime)
+        XCTAssertNotNil(dateToString, "date string is nil")
+        guard let stringDate = dateToString else {
+            XCTFail("dateToString cant be nil")
+            return
+        }
+        XCTAssert(dateString == stringDate, "somthin goes wrong")
+    }
 }
